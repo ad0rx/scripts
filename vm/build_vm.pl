75fcb9d8c7a3600bb712862a73430102f2fcc57a
Brad Whitlock <bradley.whitlock@gmail.com>
2019-05-23 16:47:53 -0600

    initial import

---
+++ vm/build_vm.pl
@@ -0,0 +1,209 @@
+use IPC::Run 'run';
+use warnings;
+
+print "Running...\n";
+
+$VBOXNAME="Test-VM-3";
+
+$VBOXMANAGE="d:/bwhitlock/virtualbox5.1.22/vboxmanage.exe";
+$VBOX_GUEST_ADDITIONS="d:/bwhitlock/virtualbox5.1.22/VBoxGuestAdditions.iso";
+
+#$VM_BASE_FOLDER="d:/bwhitlock/scripted-vbox";
+$VM_BASE_FOLDER="c:/vm";
+$VM_MEMORY_SIZE="8192";
+$VM_VRAM_SIZE="128";
+$VM_CPUS="2";
+$VM_CPUEXECUTION_CAP="90";
+
+#$UBUNTU_ISO="D:/bwhitlock/downloads/vm_support/ubuntu-16.04.4-desktop-amd64.iso";
+$UBUNTU_ISO="c:/vm/ubuntu-16.04.4-desktop-amd64.iso";
+
+$VM_HDD_FILENAME="${VBOXNAME}-hdd.vdi";
+$VM_HDD="$VM_BASE_FOLDER/$VM_HDD_FILENAME";
+
+$VM_SHARED_FOLDER="d:/bwhitlock/scripted-vbox";
+$VM_DOWNLOADS="d:/bwhitlock/downloads";
+
+sub vm_is_running
+{
+    my $vm_name = shift;
+
+    run [ $VBOXMANAGE, "showvminfo", $vm_name ], ">", \my $stdout;
+
+    #print $stdout, "\n";
+    
+    if ( $stdout =~ /State:\s*(running)/ )
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+sub createvm
+{
+    print "Creating VM\n";
+    system ($VBOXMANAGE, "--version");
+    system ($VBOXMANAGE, "createvm",
+            "--name",    $VBOXNAME,
+            "--basefolder", $VM_BASE_FOLDER,
+            "--ostype",  "Ubuntu_64",
+            "--register",
+        );
+
+    system ($VBOXMANAGE, "modifyvm",
+            $VBOXNAME,
+            "--memory", $VM_MEMORY_SIZE,
+            "--vram",   $VM_VRAM_SIZE,
+            "--cpus",   $VM_CPUS,
+            "--cpuexecutioncap", $VM_CPUEXECUTION_CAP,
+            "--pae",      "on",
+            "--hwvirtex", "on",
+            "--paravirtprovider", "default",
+            "--accelerate3d",      "on",
+            "--accelerate2dvideo", "off",
+            "--boot1", "dvd",
+            "--boot2", "disk",
+
+        );
+
+    system ($VBOXMANAGE,  "modifyvm",
+            $VBOXNAME,
+            "--nic1",     "bridged",
+            "--nictype1", "82540EM",
+            "--bridgeadapter1", "Intel(R) Ethernet Connection I219-LM",
+        );
+
+
+    system ($VBOXMANAGE, "modifyvm",
+            $VBOXNAME,
+            "--clipboard",    "bidirectional",
+            "--usb",          "on",
+            "--audio",        "none",
+        );
+
+    system ($VBOXMANAGE, "storagectl",
+            $VBOXNAME,
+            "--name",        "IDE",
+            "--add",         "ide",
+            "--bootable",    "on",
+            "--hostiocache", "off",
+        );
+
+    system ($VBOXMANAGE, "storagectl",
+            $VBOXNAME,
+            "--name",     "SATA",
+            "--add",      "sata",
+            "--bootable", "on",
+
+        );
+
+    system ($VBOXMANAGE, "storageattach",
+            $VBOXNAME,
+            "--storagectl", "IDE",
+            "--port",       "1",
+            "--device",     "1",
+            "--type",       "dvddrive",
+            "--medium",     $UBUNTU_ISO,
+        );
+
+    system ($VBOXMANAGE, "createmedium",
+            "disk",
+            "--filename", $VM_HDD,
+            "--size",     "102400",
+            "--format",   "vdi",
+        );
+
+    system ($VBOXMANAGE, "storageattach",
+            $VBOXNAME,
+            "--storagectl", "SATA",
+            "--port",       "0",
+            "--device",     "0",
+            "--type",       "hdd",
+            "--medium",     $VM_HDD,
+        );
+
+    system ($VBOXMANAGE, "startvm",
+            $VBOXNAME,
+        );
+}
+
+sub vboxadditions
+{
+
+    print "VBox Additions\n";
+    
+    system ($VBOXMANAGE, "storageattach",
+            $VBOXNAME,
+            "--storagectl", "IDE",
+            "--port",       "1",
+            "--device",     "1",
+            "--type",       "dvddrive",
+            "--medium",     $VBOX_GUEST_ADDITIONS,
+        );
+
+
+    print "\n\n** When system boots, cd /media/user/VBOX~; sudo sh ./VBoxL~; then shutdown **\n\n";
+
+    system ($VBOXMANAGE, "startvm",
+            $VBOXNAME,
+        );
+
+}
+
+sub finalconfig
+{
+    print "Final Config\n";
+    
+    # Unmount Guest Additions DVD
+    system ($VBOXMANAGE, "storageattach",
+            $VBOXNAME,
+            "--storagectl", "IDE",
+            "--port",       "1",
+            "--device",     "1",
+            "--type",       "dvddrive",
+            "--medium",     "none",
+        );
+
+    system ($VBOXMANAGE, "sharedfolder", "add",
+            $VBOXNAME,
+            "--name",    "sharedfolder",
+            "--hostpath", $VM_SHARED_FOLDER,
+            "--automount",
+        );
+
+    system ($VBOXMANAGE, "sharedfolder", "add",
+            $VBOXNAME,
+            "--name",    "downloads",
+            "--hostpath", $VM_DOWNLOADS,
+            "--automount",
+        );
+
+    system ($VBOXMANAGE, "startvm",
+            $VBOXNAME,
+        );
+
+}
+
+sub wait_till_shutdown
+{
+    print "Waiting for machine to shutdown\n";
+    while (vm_is_running $VBOXNAME)
+    {
+        sleep 3;
+    };
+
+    print "Waiting for machine to close\n";
+    sleep 10;
+}
+
+######################################################################
+######################################################################
+
+createvm;
+wait_till_shutdown;
+vboxadditions;
+wait_till_shutdown;
+finalconfig;
+
+print "Exiting.\n";
---
+++ vm/configure_vm.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+#
+# Run on the VM
+# cd /media/sf_sharedfolder ; sudo xterm -e vm.sh
+
+PETALINUX=/media/sf_downloads/petalinux-v2018.3-final-installer.run
+PETALINUX_DIR=/app/petalinux/2018.3
+SDX=/media/sf_downloads/Xilinx_SDx_2018.3_1207_2324/xsetup
+
+# Add user to vboxsf group
+T=$(groups user | grep vboxsf)
+if  [ ! "$T" ]
+then
+    
+    echo "Setting up groups"
+    sleep 2
+
+    usermod -aG vboxsf user
+    usermod -aG sudo user
+    
+    groupadd xilinx
+    usermod -aG xilinx user
+
+    # Get rcfiles
+    sudo -u user wget https://raw.githubusercontent.com/ad0rx/rcfiles/master/.bashrc
+    sudo -u user mv .bashrc /home/user/
+    sudo -u user wget https://raw.githubusercontent.com/ad0rx/rcfiles/master/.screenrc 
+    sudo -u user mv .screen /home/user/
+
+    # Get scripts
+    sudo -u user git clone https://github.com/ad0rx/scripts.git /home/user/scripts
+
+    cp /media/sf_sharedfolder/support/sudoers /etc/sudoers
+
+    # disable screen lock
+    sudo -u user gsettings set org.gnome.desktop.screensaver lock-enabled false
+    
+    echo "Logout and relogin, and rerun this script"
+    exit
+    
+fi
+
+#sleep 3
+# Update the system
+#apt update && apt upgrade -y
+
+# Install Packages
+USER_PKGS=(emacs
+	   firefox
+	   ntp
+           xinetd
+	   tftp
+	   tftpd
+	   gkrellm
+	   screen)
+for i in "${USER_PKGS[@]}"
+do
+    echo
+    echo $i
+    echo
+    #sleep 1
+    apt install -y $i
+
+done
+
+# Install Xilinx Deps
+XILINX_PKGS=(gcc
+	     gawk
+	     tofrodos
+	     tofrodos
+	     xvfb
+	     gcc
+	     make
+	     libncurses5-dev
+	     zlib1g-dev
+	     zlib1g-dev:i386
+	     libssl-dev
+	     flex
+	     bison
+	     diffstat
+	     chrpath
+	     socat
+	     xterm
+	     autoconf
+	     libtool
+	     unzip
+	     texinfo
+	     gcc-multilib
+	     build-essential
+	     libsdl1.2-dev
+	     git
+	     pax)
+for i in "${XILINX_PKGS[@]}"
+do
+
+    #echo $i
+    apt install -y $i
+
+done
+
+# NTP
+timedatectl set-ntp yes
+
+#sudo -u user gkrellm &
+
+# Xilinx Install Area
+mkdir -p /app
+chown -R user:xilinx /app
+chmod -R g+s /app
+chmod -R 775 /app
+
+# install vivado
+#sudo -u user nice -n 20 $SDX &
+
+#install petalinux
+sudo dpkg-reconfigure dash
+mkdir /tftpboot
+chmod 777 /tftpboot
+chown -R nobody /tftpboot
+cp support/tftp /etc/xinetd.d/
+service xinetd restart
+mkdir -p $PETALINUX_DIR
+chown -R user:xilinx /app
+chmod -R 777 /app
+pushd /home/user
+sudo -u user $PETALINUX $PETALINUX_DIR
+popd
+
+wait
+# Set permissions on /app
+chown -R user:xilinx /app
+chmod -R 755 /app
+
