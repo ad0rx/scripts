b89a7ca49fa09226505e7a18b9b6ae0c54cd7a1d
Brad <bradley.whitlock@gmail.com>
2019-09-05 15:15:48 -0600

    added logs

--- putty.pl
+++ putty.pl
@@ -5,7 +5,10 @@ use warnings;
 #1500 is right side monitor
 
 $TTERMPRO="C:\\Program Files (x86)\\teraterm\\ttermpro.exe";
+$LOG6="C:\\Users\\045592\\Desktop\\tt-6-log.txt";
+$LOG7="C:\\Users\\045592\\Desktop\\tt-7-log.txt";
+$LOG8="C:\\Users\\045592\\Desktop\\tt-8-log.txt";
 
-system 1, $TTERMPRO, "/C=8", "/BAUD 115200", "/x=455", "/y=0";
-system 1, $TTERMPRO, "/C=6", "/BAUD 115200", "/x=1500", "/y=0";
-system 1, $TTERMPRO, "/C=7", "/BAUD 115200", "/x=1500", "/y=400";
+system 1, $TTERMPRO, "/C=8", "/BAUD 115200", "/x=455", "/y=0", "/L=$LOG8";
+system 1, $TTERMPRO, "/C=6", "/BAUD 115200", "/x=1500", "/y=0", "/L=$LOG6";
+system 1, $TTERMPRO, "/C=7", "/BAUD 115200", "/x=1500", "/y=400", "/L=$LOG7";
