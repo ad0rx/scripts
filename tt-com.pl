de0461407acdbb66fa40ee0ea32eeab03841a62f
Bradley Whitlock <bradley.whitlock@gmail.com>
2019-10-31 11:41:04 -0600

    fixed path to log

--- tt-com.pl
+++ tt-com.pl
@@ -6,9 +6,9 @@ use warnings;
 #1500 is right side monitor
 
 $TTERMPRO="C:\\Program Files (x86)\\teraterm\\ttermpro.exe";
-$LOG6="D:/bwhitlock/projects/teraterm_logs/COM6-log.txt";
-$LOG7="D:/bwhitlock/projects/teraterm_logs/COM7-log.txt";
-$LOG8="D:/bwhitlock/projects/teraterm_logs/COM8-log.txt";
+$LOG6="D:\\bwhitlock\\projects\\teraterm_logs\\COM6-log.txt";
+$LOG7="D:\\bwhitlock\\projects\\teraterm_logs\\COM7-log.txt";
+$LOG8="D:\\bwhitlock\\projects\\teraterm_logs\\COM8-log.txt";
 
 system 1, $TTERMPRO, "/C=8", "/BAUD 115200", "/x=455", "/y=0", "/L=$LOG8";
 system 1, $TTERMPRO, "/C=6", "/BAUD 115200", "/x=1500", "/y=0", "/L=$LOG6";
