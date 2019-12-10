08054e0cde6b9214aa3bc7c0c95d9a1817ef4e3b
Brad Whitlock <bradley.whitlock@gmail.com>
2019-12-10 11:38:47 -0700

    initial

---
+++ vncstart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Start a VNC session
+ME=$(basename $0)
+#GEOM="1920x1080"
+GEOM="1900x960"
+DEPTH=16
+USER=bwhitlock
+
+echo; echo "${ME} Running"
+
+# If an $1 was given, it was the geometry
+if [ -n "$1" ]
+then
+    echo; echo "Setting Geometry to ${1}"
+    GEOM="$1"
+fi
+
+#cd /home/${USER}
+sudo -u ${USER} vncserver -geometry ${GEOM} -depth ${DEPTH} -rfbport 5901 -localhost 
+
+echo; echo "${ME} Complete"
