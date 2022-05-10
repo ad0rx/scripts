74f4a7c28a2eac71601f2eb84e3ec6dae38fd500
Bradley Whitlock <bradley.whitlock@gmail.com>
2022-05-09 21:28:02 -0600

    simplified, updated to work on bdmca project

--- verilog-etags.sh
+++ verilog-etags.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
+# From some website:
 #etags --language=none \
 #  --regex='/[ \t]*\(`define\)[ \t]+\([^ \t]+\)/`\2/' \
 #  --regex='/[ \t]*\(module\)[ \t]+\([^ \t]+\)/\2/' \
@@ -7,7 +8,20 @@
 #  ../../*/include/*.v ../../include/*.v ../../../../include/*.v *.v
 #  --output=TAGS
 
-find $1 -regex '.*[.][s]?v[h]?' | xargs etags -a -o TAGS \
-                                        --regex='/[ \t]*\(`define\)[ \t]+\([^ \t]+\)/`\2/' \
-                                        --regex='/[ \t]*\(module\)[ \t]+\([^ \t]+\)/\2/' \
-                                        --regex='/[ \t]*\(input\|output\|reg\|wire\|function\)[ \t]+\(\[[^]]+\][ \t]+\)?\([^ \t]+\)/\3/'
+# Default to anchoring the find command in the current dir
+ANCHOR=`pwd`
+if [ -n "$1" ]
+then
+    echo "Anchoring find to $1"
+    ANCHOR=$1
+fi
+
+rm TAGS
+find $ANCHOR -regex '.*[.][s]?v[h]?' |                                \
+    xargs etags --language=none -a -o TAGS                            \
+          --regex='/[ \t]*\(`define\)[ \t]+\([^ \t]+\)/`\2/'          \
+          --regex='/[ \t]*\(module\)[ \t]+\([^ \t]+\)/\2/'            \
+          --regex='/[ \t]*\(class\)[ \t]+\([^ \t;]+\)/\2/'            \
+          --regex='/[ \t]*\(package\)[ \t]+\([^ \t;]+\)/\2/'          \
+          --regex='/[ \t]*\(interface\)[ \t]+\([^ \t;(]+\)/\2/'       \
+          --regex='/[ \t]*\(task\)[ \t]+\([^ \t;(]+\)/\2/'            \
