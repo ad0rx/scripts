200f2808471f9366fca7b585b4172ad0868ebb40
Bradley Whitlock <bradley.whitlock@peraton.com>
2022-07-27 16:07:30 -0400

    added checks for active sessions

--- vpn.sh
+++ vpn.sh
@@ -4,20 +4,46 @@ CONFIGS_DIR=/home/bwhitlock/03_peraton_laptop/openvpn/config
 CONFIG=brg-lab-tcp-per.ovpn
 #
 # One of the autoload has autostart: true
+# while read line; do openvpn3 session-manage -D --path ${line/Path: /}; done < p
+function get_paths () {
+
+    PATHS=`openvpn3 sessions-list | grep Path`
+    PATHS=${PATHS//Path: /}
+    #echo "Paths: "
+    #echo $PATHS
+}
 
 if [ -n "$1" ]
 then
-    openvpn3 session-manage -D -c ${CONFIG}
+    get_paths
+    for path in ${PATHS}
+    do
+        openvpn3 session-manage -D --path $path
+    done
+
+    echo "Current Sessions"
+    openvpn3 sessions-list
+
     exit
 else
 
 
-# Start the connection
-#openvpn3 session-start --config ${CONFIGS_DIR}/${CONFIG}
-openvpn3-autoload --directory ${CONFIGS_DIR}
+    # Make sure no current sessions
+    get_paths
+    if [ -n "$PATHS" ]
+    then
+        echo "Sessions:"
+        echo $PATHS
+        echo "active session...exiting"
+        exit
+    fi
 
-# Remove from config manager to keep that clean, it's not necessary to
-# keep this in the config space
-openvpn3 config-remove --force -c ${CONFIG}
+    # Start the connection
+    #openvpn3 session-start --config ${CONFIGS_DIR}/${CONFIG}
+    openvpn3-autoload --directory ${CONFIGS_DIR}
+
+    # Remove from config manager to keep that clean, it's not necessary to
+    # keep this in the config space
+    openvpn3 config-remove --force -c ${CONFIG}
 
 fi
