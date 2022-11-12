891d92fb3dfd6f5324e609b3b44250b070eec9a2
Bradley Whitlock <bradley.whitlock@peraton.com>
2022-11-11 21:16:43 -0500

    various updates

--- vpn.sh
+++ vpn.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 # Start the VPN to peraton labs
-CONFIGS_DIR=/home/bwhitlock/03_peraton_laptop/openvpn/config
-CONFIG=brg-lab-tcp-per.ovpn
 #
 # One of the autoload has autostart: true
+#
+#######################################################################
+
+CONFIGS_DIR=/home/bwhitlock/03_peraton_laptop/openvpn/config
+CONFIG=brg-lab-tcp-per.ovpn
+
 # while read line; do openvpn3 session-manage -D --path ${line/Path: /}; done < p
 function get_paths () {
 
     PATHS=`openvpn3 sessions-list | grep Path`
     PATHS=${PATHS//Path: /}
-    #echo "Paths: "
-    #echo $PATHS
+
 }
 
-if [ -n "$1" ]
-then
+function remove_sessions () {
+
     get_paths
     for path in ${PATHS}
     do
@@ -24,19 +27,39 @@ then
     echo "Current Sessions"
     openvpn3 sessions-list
 
-    exit
-else
+}
+#
+# One of the autoload has autostart: true
+#
+#######################################################################
 
+function test_connection () {
 
-    # Make sure no current sessions
-    get_paths
-    if [ -n "$PATHS" ]
+    PING_OUTPUT=$(ping -W 1 -c 1 hc14)
+    RETURN=$?
+    echo "Ping Return: $RETURN"
+
+    if [ "$RETURN" -eq "1" ]
     then
-        echo "Sessions:"
-        echo $PATHS
-        echo "active session...exiting"
-        exit
+        echo "Connection is not functional"
     fi
+
+    return $RETURN
+}
+
+function remove_configs () {
+
+    local configs=`openvpn3 configs-list | grep configuration`
+
+    for config in $configs
+    do
+        echo "Removing config $config"
+        openvpn3 config-remove --force --path $config
+    done
+
+}
+
+function start_new_connection () {
 
     # Start the connection
     #openvpn3 session-start --config ${CONFIGS_DIR}/${CONFIG}
@@ -44,6 +67,29 @@ else
 
     # Remove from config manager to keep that clean, it's not necessary to
     # keep this in the config space
-    openvpn3 config-remove --force -c ${CONFIG}
+    #openvpn3 config-remove --force -c ${CONFIG}
+    remove_configs
+}
+
+touch /home/bwhitlock/03_peraton_laptop/00_vpn.log
+
+# Begin Main Program Execution
+if [ -n "$1" ]
+then
+    remove_sessions
+    exit
+else
+
+    if test_connection
+    then
+        # Connection Up
+        echo "Current Connection Functional"
+    else
+        # Connection Down
+        echo "Current Connection non-Functional"
+
+        remove_sessions
+        start_new_connection
+    fi
 
 fi
